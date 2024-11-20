9859074ea6e451abd7cdaf6e408ded1e2d2ad92b
davidramos1121 <152838084+davidramos1121@users.noreply.github.com>
2024-11-20 14:16:04 -0500

    cambio xs
    cambio xs

--- Diseños/interfazPrincipal.py
+++ Diseños/interfazPrincipal.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'interfazprincipal.ui'
+# Form implementation generated from reading ui file 'interfazPrincipal.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.9
 #
@@ -18,18 +18,36 @@ class Ui_MainWindow(object):
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(210, 20, 471, 91))
+        self.label.setGeometry(QtCore.QRect(180, 40, 451, 81))
         font = QtGui.QFont()
-        font.setFamily("Sitka")
-        font.setPointSize(22)
+        font.setFamily("MS Reference Sans Serif")
+        font.setPointSize(26)
         self.label.setFont(font)
         self.label.setObjectName("label")
         self.toolButton = QtWidgets.QToolButton(self.centralwidget)
-        self.toolButton.setGeometry(QtCore.QRect(80, 130, 661, 41))
+        self.toolButton.setGeometry(QtCore.QRect(110, 240, 571, 51))
         font = QtGui.QFont()
-        font.setPointSize(14)
+        font.setPointSize(16)
         self.toolButton.setFont(font)
         self.toolButton.setObjectName("toolButton")
+        self.toolButton_2 = QtWidgets.QToolButton(self.centralwidget)
+        self.toolButton_2.setGeometry(QtCore.QRect(110, 150, 571, 51))
+        font = QtGui.QFont()
+        font.setPointSize(16)
+        self.toolButton_2.setFont(font)
+        self.toolButton_2.setObjectName("toolButton_2")
+        self.toolButton_3 = QtWidgets.QToolButton(self.centralwidget)
+        self.toolButton_3.setGeometry(QtCore.QRect(110, 320, 571, 51))
+        font = QtGui.QFont()
+        font.setPointSize(16)
+        self.toolButton_3.setFont(font)
+        self.toolButton_3.setObjectName("toolButton_3")
+        self.toolButton_4 = QtWidgets.QToolButton(self.centralwidget)
+        self.toolButton_4.setGeometry(QtCore.QRect(110, 410, 571, 51))
+        font = QtGui.QFont()
+        font.setPointSize(16)
+        self.toolButton_4.setFont(font)
+        self.toolButton_4.setObjectName("toolButton_4")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(MainWindow)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 800, 26))
@@ -46,7 +64,10 @@ class Ui_MainWindow(object):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
         self.label.setText(_translate("MainWindow", "Facturacion Agricola"))
-        self.toolButton.setText(_translate("MainWindow", "Registrar Cliente"))
+        self.toolButton.setText(_translate("MainWindow", "Realizar compra"))
+        self.toolButton_2.setText(_translate("MainWindow", "Registrar Cliente"))
+        self.toolButton_3.setText(_translate("MainWindow", "Buscar Cliente por numero de cedula"))
+        self.toolButton_4.setText(_translate("MainWindow", "Salir"))
 
 
 if __name__ == "__main__":
--- GUI/Gui.py
+++ GUI/Gui.py
@@ -1,1 +1,13 @@
-from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5 import QtWidgets, QtCore
+from PyQt5.QtWidgets import QMessageBox
+from Diseños.interfazPrincipal import Ui_MainWindow
+
+
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
